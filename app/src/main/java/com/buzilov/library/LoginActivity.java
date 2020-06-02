fb8d4218847d227680e2b822632672adc9203380
Olexij <alexbuzilov.official@gmail.com>
2020-06-03 00:13:57 +0300

    -- Added Firebase to the project. -- Implemented a simple auth and registration.

--- app/src/main/java/com/buzilov/library/LoginActivity.java
+++ app/src/main/java/com/buzilov/library/LoginActivity.java
@@ -1,14 +1,46 @@
 package com.buzilov.library;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private EditText emailText, passwordText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailText = findViewById(R.id.loginEmail);
+        passwordText = findViewById(R.id.loginPassword);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void createAccount(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    public void login(View view) {
+        String email = emailText.getText().toString(),
+                password = passwordText.getText().toString();
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener((task) -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(this, "Successfully logged in!", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(this, MainActivity.class));
+                    } else {
+                        Toast.makeText(this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/buzilov/library/RegisterActivity.java
+++ app/src/main/java/com/buzilov/library/RegisterActivity.java
@@ -2,13 +2,47 @@ package com.buzilov.library;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private EditText emailText, displayNameText, passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        emailText = findViewById(R.id.registerEmailText);
+        displayNameText = findViewById(R.id.registerDisplayNameText);
+        passwordText = findViewById(R.id.registerPasswordText);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void register(View view) {
+        String email = emailText.getText().toString(),
+                displayName = displayNameText.getText().toString(),
+                password = passwordText.getText().toString();
+
+         firebaseAuth.createUserWithEmailAndPassword(email, password)
+                 .addOnCompleteListener((resultTask) -> {
+                     if (resultTask.isSuccessful()) {
+                         Toast.makeText(this, "Successfully registered!", Toast.LENGTH_SHORT).show();
+                         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                     } else {
+                        Toast.makeText(this, "Error!" + resultTask.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                     }
+                 });
+
     }
 }
